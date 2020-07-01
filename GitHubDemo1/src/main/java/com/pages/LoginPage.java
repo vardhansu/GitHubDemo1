ce269f0d250f5939b04a4bdc9242d321bb735be3
vardansharwani <vardan.sharwani@gmail.com>
2020-07-01 17:17:24 +0530

    First Commit

---
+++ GitHubDemo1/src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package com.pages;
+
+public class LoginPage {
+
+	public void loginPage() {
+		
+		System.out.println("This is login page");
+		
+		
+	}
+	
+}
---
+++ GitHubDemo1/src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,11 @@
+package com.testcases;
+
+public class LoginTest {
+
+	
+public void loginTest() {
+		
+		System.out.println("This is login test");
+		
+	}
+}
